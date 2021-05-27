cf449c866e54fdb9af856bdfeff4199234eec846
nranouar <nourelhadi@mobiblanc.com>
2021-05-27 11:57:30 +0100

    add and enable bootstrap 5 && re config webpack && add static home page with fake content

--- src/UI/Controller/HomeController.php
+++ src/UI/Controller/HomeController.php
@@ -4,12 +4,13 @@ namespace App\UI\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class HomeController
 {
     #[Route(path: '/', name: 'app_home')]
-    public function __invoke(): Response
+    public function __invoke(Environment $twig): Response
     {
-        return new Response('OK Clean Architecture');
+        return new Response($twig->render('home/index.html.twig'));
     }
 }
